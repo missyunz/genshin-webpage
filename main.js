d3954a7a0c761965bed9af698a4f16f79d87229e
missyunz <gianella.30october@gmail.com>
2025-04-15 18:54:23 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {  
+    const content = document.querySelector('.main-content');  
+    const aside = document.querySelector('.latest-update');  
+
+    content.classList.add('fade-in');  
+    aside.classList.add('fade-in');  
+}); 
